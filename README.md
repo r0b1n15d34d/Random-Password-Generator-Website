21112bbb0d22a6b89a3c90c41f03bc57c68107b5
r0b1n <l0st1nw0nd3rl4nd@protonmail.com>
2026-05-26 23:24:14 +0300

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Random Password Generator Website
+
+## Features
+- Generate and copy a random 12-character password
---
+++ script.js
@@ -0,0 +1,87 @@
+const btn = document.querySelector("#btn");
+const Label = document.querySelector("#Label");
+const instructionText = document.querySelector("#instruction-text");
+const copyBtn = document.querySelector("#copy-btn");
+const canvas = document.getElementById("Matrix");
+const context = canvas.getContext("2d");
+
+
+
+// for generating the password
+const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*'; 
+let password = "";
+let char = ""
+
+function generatePassword() {
+	instructionText.style.display = 'none';
+
+    password = ""
+     for (let i = 0; i < 12; i++) {
+        char = Math.floor(Math.random() * characters.length)
+        password += characters[char]   
+    }
+
+    return Label.textContent = password;
+}
+btn.addEventListener("click", generatePassword)
+
+
+function copyPassword () {
+	if (password) {
+		navigator.clipboard.writeText(password)
+		copyBtn.textContent = "Copied!";
+		setTimeout(() => copyBtn.textContent = "Copy", 2000);
+	}
+}
+
+copyBtn.addEventListener("click", copyPassword)
+
+
+
+
+
+// for the Matrix rain effect
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+
+const alphabet = katakana + latin + nums;
+
+const fontSize = 16;
+const columns = canvas.width/fontSize;
+
+const rainDrops = [];
+
+for( let x = 0; x < columns; x++ ) {
+	rainDrops[x] = 1;
+}
+
+const draw = () => {
+	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	context.fillStyle = '#0F0';
+	context.font = fontSize + 'px monospace';
+
+	for(let i = 0; i < rainDrops.length; i++)
+	{
+		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+		
+		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+			rainDrops[i] = 0;
+        }
+		rainDrops[i]++;
+	}
+	
+};
+
+setInterval(draw, 45);
+
+
+
+
+
